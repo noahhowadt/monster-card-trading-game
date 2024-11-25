8ace58c56ccbcfe1ac5da29753a62d0787ea9906
Noah Howadt <noah.howadt@gmail.com>
2024-11-25 12:53:18 +0100

    print all errors

--- src/main/java/at/technikum/application/mctg/exceptions/ExceptionHandler.java
+++ src/main/java/at/technikum/application/mctg/exceptions/ExceptionHandler.java
@@ -10,22 +10,25 @@ public class ExceptionHandler {
         try {
             return func.get();
         } catch (BadRequestException e) {
-            return this.createErrorResponse(Status.BAD_REQUEST);
-        } catch (NotFoundException e) {
-            return this.createErrorResponse(Status.NOT_FOUND);
-        } catch (ConflictException e) {
-            return this.createErrorResponse(Status.CONFLICT);
-        } catch (MethodNotAllowedException e) {
-            return this.createErrorResponse(Status.METHOD_NOT_ALLOWED);
-        } catch (UnauthorizedException e) {
-            return this.createErrorResponse(Status.UNAUTHORIZED);
-        } catch (RuntimeException e) {
             System.out.println(e.getMessage());
-            return this.createErrorResponse(Status.INTERNAL_SERVER_ERROR);
+            return this.createErrorResponse(e, Status.BAD_REQUEST);
+        } catch (NotFoundException e) {
+            return this.createErrorResponse(e, Status.NOT_FOUND);
+        } catch (ConflictException e) {
+            return this.createErrorResponse(e, Status.CONFLICT);
+        } catch (MethodNotAllowedException e) {
+            return this.createErrorResponse(e, Status.METHOD_NOT_ALLOWED);
+        } catch (UnauthorizedException e) {
+            return this.createErrorResponse(e, Status.UNAUTHORIZED);
+        } catch (ForbiddenException e) {
+            return this.createErrorResponse(e, Status.FORBIDDEN);
+        } catch (RuntimeException e) {
+            return this.createErrorResponse(e, Status.INTERNAL_SERVER_ERROR);
         }
     }
 
-    private Response createErrorResponse(Status status) {
+    private Response createErrorResponse(RuntimeException e, Status status) {
+        System.out.println(e.getMessage());
         Response res = new Response();
         res.setStatus(status);
         return res;
